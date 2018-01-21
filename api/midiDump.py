ae749528563b53ad7611349309bd7827ca9273e0
Christopher Misa <cmisa@cs.uoregon.edu>
2018-01-20 22:30:21 -0800

    midiDump now mapping into event chunks

--- api/midiDump.py
+++ api/midiDump.py
@@ -5,6 +5,8 @@ import struct
 import sys
 import mmap #qSvE tag of arrangement data chunk
 ARRANGMENT_CHUNK_TAG = "\x71\x53\x76\x45\x01\x00\x17\x00\x00\x00\x04"
+EVENT_CHUNK_HEADER_TAG = "\x71\x65\x53\x4d\x02\x00\x17\x00\x00\x00"
+EVENT_CHUNK_TAG = "\x71\x53\x76\x45\x01\x00\x17\x00\x00\x00"
 NO_LOOP_VALUE = 0x3FFFFFFF00000000
 START_TIME_OFFSET = 0x8700
 
@@ -35,13 +37,28 @@ def decodeArrEvent(e):
     Interprits the arrangement chunk's event structs
   """
   eventType, startTime, trackID, loopTime, eventID = \
-     struct.unpack("<L L 12x B 3x Q L", e[0:36])
+     struct.unpack("<L L 12x B 3x Q 4s", e[0:36])
   # Only the upper 6 bytes of loopTime seem to mater
   loopTime = loopTime >> 16 if loopTime != NO_LOOP_VALUE else None
   # Normalize start times
   startTime -= START_TIME_OFFSET
   return {"type":eventType, "start":startTime, "track_id":trackID, \
           "loop_time":loopTime, "id":eventID}
+
+def getEventChunk(pd, eventID, startOffset = 0):
+  """
+    Finds and returns the event chunk with the given id from the mmap'ed
+    ProjectData file
+    Returns a tuple with first element as qeSM header chunk and second
+    as qSvE body chunk
+  """
+  eventHeaderAddr = pd.find(EVENT_CHUNK_HEADER_TAG + eventID, startOffset)
+  eventBodyAddr = pd.find(EVENT_CHUNK_TAG + eventID, eventHeaderAddr)
+  (chunkSize,) = struct.unpack("<Q", pd[eventBodyAddr+28:eventBodyAddr+36])
+  eventHeader = pd[eventHeaderAddr:eventBodyAddr]
+  eventBody = pd[eventBodyAddr+36:eventBodyAddr+36+chunkSize]
+  return (eventHeader, eventBody)
+  
 
 def main():
   """
@@ -51,7 +68,8 @@ def main():
     mm = mmap.mmap(f.fileno(), 0)
     events = decodeArrChunk(getArrChunk(mm))
     for e in events:
-      print(str(e))
+      h,b = getEventChunk(mm,e['id'])
+      sys.stdout.write(h + "\xEE" * 16 + b + "\xFF"*16)
 
 if __name__ == "__main__":
   main()
