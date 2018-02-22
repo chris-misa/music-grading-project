eae66300d00478571cfea2ff06c7a64f837dd3da
chris-misa <chris@chrismisa.com>
2018-02-22 15:52:36 -0800

    Chapter 4.2

--- chapters/ch4.py
+++ chapters/ch4.py
@@ -27,15 +27,23 @@ def hasCorrectInstruments(bf):
     if not track:
       return False
     for word in words:
+      # Check keywords
       if track['keywords']:
         for k in track['keywords']:
           if word in k:
             trackOk = True
             break
+      # Check label
       if track['label']:
         if word in track['label']:
           trackOk = True
           break
+      # Check region names
+      if 'regions' in track.keys():
+        for r in track['regions']:
+          if word in r['name']:
+            trackOk = True
+            break
     if not trackOk:
       return False
   # Make sure last track is not bass
@@ -51,6 +59,14 @@ def hasCorrectInstruments(bf):
       return False
   return True
 
+def trackOneIsEmpty(bf):
+  """
+    Returns true if track one is empty
+  """
+  if 'regions' in bf['tracks'][1].keys():
+    return len(bf['tracks'][1]['regions']) == 0
+  return True
+
 def test(fp):
   """
     Runs chapter 4 tests against the given gb project
@@ -58,7 +74,10 @@ def test(fp):
   failedCodes = []
   bf = bandFile.load(fp)
   if not hasCorrectInstruments(bf):
-    failedCodes.append(TestCode(4,1,description="Wrong tracks"))
+    failedCodes.append(TestCode(4,1,description="Wrong track layout"))
+
+  if not trackOneIsEmpty(bf):
+    failedCodes.append(TestCode(4,2,description="Track one is not empty"))
 
   if failedCodes:
     raise TestError(failedCodes)
